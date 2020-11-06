c643e091a0eec277c122586a50284e7c454e43f9
Yao Weijian <wj.yao@uctechip.com>
2020-11-06 17:37:24 +0800

    [software/u8g2_demo]: Create the project and copy the C source of u8g2 to csrc. In order to minimize the size of code, only copy the ssd1309 driver.

---
+++ software/u8g2_demo/u8g2_demo.c
@@ -0,0 +1,22 @@
+/*********************************************************************************
+*
+*       PROJECT:        WH32_DDR 
+* 
+*       FILENAME:       u8g2_demo.c 
+* 
+*       FUNCTION:       u8g2_demo driver 
+* 
+*       AUTHOR:         viking 
+* 
+*       DATE:           2020年 11月 06日 星期五 17:25:52 CST 
+* 
+*       ISFINISH:       NO 
+* 
+*       STATUS:  
+*********************************************************************************/
+#include "platform.h"
+int main(void)
+{
+    /* write your code here */
+    return 0;
+}
